e4a1245a34c645da04c7f2d51618a36bf78d1b86
shefat2002 <shefat2002@gmail.com>
2025-10-13 12:09:04 +0600

    Add project files.

---
+++ EmployeeManagementSystemWebApi/Models/EmployeeDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeManagementSystemWebApi.Models;
+
+public class EmployeeDbContext : DbContext
+{
+    public EmployeeDbContext(DbContextOptions<EmployeeDbContext> options) : base(options)
+    {
+    }
+    public DbSet<Employee> Employees { get; set; } = null!;
+}
---
+++ EmployeeManagementSystemWebApi/Program.cs
@@ -0,0 +1,49 @@
+using EmployeeManagementSystemWebApi.Models;
+using Microsoft.AspNetCore.Mvc.Formatters;
+using Microsoft.EntityFrameworkCore;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+builder.Services.AddSwaggerGen();
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<EmployeeDbContext>(options =>
+    options.UseSqlServer(connectionString));
+
+builder.Services.AddControllers(options =>
+{
+    options.OutputFormatters.RemoveType<SystemTextJsonOutputFormatter>();
+    options.OutputFormatters.Add(new SystemTextJsonOutputFormatter(new JsonSerializerOptions
+    {
+        ReferenceHandler = ReferenceHandler.IgnoreCycles,
+        PropertyNameCaseInsensitive = false,
+        PropertyNamingPolicy = null,
+        WriteIndented = true,
+        TypeInfoResolver = JsonSerializerOptions.Default.TypeInfoResolver
+    }));
+});
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+// Configure the HTTP request pipeline.
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
