3b7e0eae6dd909a4297e917d1097406c6435fecd
shefat2002 <shefat2002@gmail.com>
2025-10-31 01:25:46 +0600

    removed unnecessary projects

--- EmployeeMVC_Consume/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace EmployeeMVC_Consume.Models
-{
-   public class ErrorViewModel
-   {
-      public string? RequestId { get; set; }
-
-      public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-   }
-}
--- EmployeeMVC_Consume/ViewModels/JobRoleVM.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace EmployeeMVC_Consume.ViewModels;
-
-public class JobRoleVM
-{
-    public int Id { get; set; }
-    public string JobTitle { get; set; } = string.Empty;
-    public string? Description { get; set; }
-    public int BaseSalary { get; set; }
-}
